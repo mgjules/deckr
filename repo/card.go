5f4a1e3b9b4d4de2dba6c4eaabda6d6ebe157ce4
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-05-13 12:02:43 +0400

    style: remove unnecessary struct field tags

--- repo/card.go
+++ repo/card.go
@@ -2,7 +2,7 @@ package repo
 
 // Card represents a card.
 type Card struct {
-	Rank string `json:"rank"`
-	Suit string `json:"suit"`
-	Code string `json:"code"`
+	Rank string
+	Suit string
+	Code string
 }
--- repo/deck.go
+++ repo/deck.go
@@ -2,7 +2,7 @@ package repo
 
 // Deck represents a deck of cards.
 type Deck struct {
-	ID       string `json:"id"`
-	Shuffled bool   `json:"shuffled"`
-	Cards    []Card `json:"cards"`
+	ID       string
+	Shuffled bool
+	Cards    []Card
 }
